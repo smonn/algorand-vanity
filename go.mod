e383d0f7eb47dd84205bca4ac9a808150ef202b1
Simon Ingeson <44818+smonn@users.noreply.github.com>
2021-11-06 20:19:18 -0400

    Improve multi-threaded support

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module smonn/algorand-vanity
+module github.com/smonn/algorand-vanity
 
 go 1.17
 
+require github.com/algorand/go-algorand-sdk v1.11.0
+
 require (
-	github.com/algorand/go-algorand-sdk v1.10.0 // indirect
 	github.com/algorand/go-codec/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"runtime"
 	"strings"
 	"time"
 
@@ -11,42 +12,74 @@ import (
 	"github.com/algorand/go-algorand-sdk/mnemonic"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Need to specify address prefix\n")
-		return
+func printStatus(countChannel chan int) {
+	var count int = 0
+	var start time.Time = time.Now()
+
+	for {
+		count += <-countChannel
+
+		var end time.Time = time.Now()
+		var diff float64 = (end.Sub(start).Seconds())
+
+		if diff >= 1 {
+			fmt.Printf("\raddrs/sec: %12d", count)
+			count = 0
+			start = time.Now()
+		}
 	}
+}
 
-	start := time.Now()
-	needle := regexp.MustCompile(strings.ToUpper(os.Args[1]))
-	account := crypto.GenerateAccount()
-
-	// Unlikely we hit more than a 32-bit integer for count as we reset every second
-	var count uint = 0
-	// Might get a lot of addresses in total
-	var total uint64 = 0
-
-	fmt.Printf("looking for prefix %s\n", needle)
-	fmt.Print("??? addr/sec")
+func findMatch(needle *regexp.Regexp, total *uint64, foundAccount chan crypto.Account, countChannel chan int) {
+	var account crypto.Account = crypto.GenerateAccount()
 
 	for {
 		if needle.MatchString(account.Address.String()) {
+			foundAccount <- account
 			break
 		}
 
 		account = crypto.GenerateAccount()
-		count += 1
-		total += 1
-		end := time.Now()
-		diff := end.Sub(start).Seconds()
+		countChannel <- 1
+		*total += 1
+	}
+}
 
-		if diff >= 1 {
-			start = time.Now()
-			fmt.Printf("\r%d addr/sec", count)
-			count = 0
-		}
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Need to specify address regexp\n")
+		return
 	}
 
+	// Max number of goroutines
+	var numCPU int = runtime.NumCPU()
+
+	// Compile regexp pattern
+	var needle *regexp.Regexp = regexp.MustCompile(strings.ToUpper(os.Args[1]))
+
+	// Might get a lot of addresses in total
+	var total uint64 = 0
+
+	// Used to store the first match
+	var foundAccount = make(chan crypto.Account)
+
+	// Track addr/sec across goroutines
+	var count = make(chan int)
+
+	fmt.Printf("looking for address using pattern '%s'\n", needle)
+	fmt.Print("??? addr/sec")
+
+	for i := 0; i < numCPU; i++ {
+		go findMatch(needle, &total, foundAccount, count)
+	}
+
+	// Start outputting the status
+	go printStatus(count)
+
+	// Wait for the first match, this will "stop" the goroutines
+	var account crypto.Account = <-foundAccount
+
+	// Newline to clear the status output
 	fmt.Println()
 	m, err := mnemonic.FromPrivateKey(account.PrivateKey)
 
